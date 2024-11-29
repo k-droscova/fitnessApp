ef7956190e6e1956d816d0b667b8ff1b4809a9cb
Karolina Droscova <drosckar@fit.cvut.cz>
2024-11-29 18:06:28 +0100

    trainee service

--- fitnessApp/src/main/java/cz/cvut/fit/tjv/fitnessApp/service/TraineeService.java
+++ fitnessApp/src/main/java/cz/cvut/fit/tjv/fitnessApp/service/TraineeService.java
@@ -2,5 +2,8 @@ package cz.cvut.fit.tjv.fitnessApp.service;
 
 import cz.cvut.fit.tjv.fitnessApp.domain.Trainee;
 
+import java.util.List;
+
 public interface TraineeService extends CrudService<Trainee, Long> {
+    List<Trainee> findTraineesByFitnessClassId(Long fitnessClassId);
 }
--- fitnessApp/src/main/java/cz/cvut/fit/tjv/fitnessApp/service/TraineeServiceImpl.java
+++ fitnessApp/src/main/java/cz/cvut/fit/tjv/fitnessApp/service/TraineeServiceImpl.java
@@ -1,10 +1,12 @@
 package cz.cvut.fit.tjv.fitnessApp.service;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
 import cz.cvut.fit.tjv.fitnessApp.domain.Trainee;
 import cz.cvut.fit.tjv.fitnessApp.repository.TraineeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class TraineeServiceImpl extends CrudServiceImpl<Trainee, Long> implements TraineeService {
@@ -19,5 +21,13 @@ public class TraineeServiceImpl extends CrudServiceImpl<Trainee, Long> implement
     @Override
     protected CrudRepository<Trainee, Long> getRepository() {
         return traineeRepository;
+    }
+
+    @Override
+    public List<Trainee> findTraineesByFitnessClassId(Long fitnessClassId) {
+        if (fitnessClassId == null) {
+            throw new IllegalArgumentException("FitnessClass ID must not be null");
+        }
+        return traineeRepository.findByFitnessClassId(fitnessClassId);
     }
 }
