ffd825707a010c96901d45a9a594fc482a680275
Karolina Droscova <drosckar@fit.cvut.cz>
2024-12-05 10:35:11 +0100

    app dependencies

--- frontend/fitnessApp/fitnessApp/App/Source/DI/AppDependencies.swift
+++ frontend/fitnessApp/fitnessApp/App/Source/DI/AppDependencies.swift
@@ -9,12 +9,41 @@ import Foundation
 
 let appDependencies = AppDependencies()
 
+typealias HasNetworking = HasNetwork & HasUrlSessionWrapper
+typealias HasAPIServices = HasClassTypeAPIService & HasFitnessClassAPIService & HasInstructorAPIService & HasRoomAPIService & HasTraineeAPIService
+typealias HasManagers = HasUserManager & HasDataManagers
+typealias HasDataManagers = HasClassTypeManager & HasFitnessClassManager & HasInstructorManager & HasRoomManager & HasTraineeManager
+typealias HasStorage = HasUserDefaultsStoraging
+
 final class AppDependencies {
+    // MARK: - logger
     lazy var logger: LoggerServicing = OSLoggerService()
+    
+    // MARK: - storage
     lazy var userDefaultsStorage: UserDefaultsStoraging = UserDefaultsStorage(dependencies: self)
+    
+    // MARK: - networking
+    lazy var network: Networking = Network(dependencies: self)
+    lazy var urlSession: URLSessionWrapping = URLSessionWrapper()
+    
+    // MARK: - API services
+    lazy var classTypeAPIService: ClassTypeAPIServicing = ClassTypeAPIService(dependencies: self)
+    lazy var fitnessClassAPIService: FitnessClassAPIServicing = FitnessClassAPIService(dependencies: self)
+    lazy var instructorAPIService: InstructorAPIServicing = InstructorAPIService(dependencies: self)
+    lazy var roomAPIService: RoomAPIServicing = RoomAPIService(dependencies: self)
+    lazy var traineeAPIService: TraineeAPIServicing = TraineeAPIService(dependencies: self)
+    
+    // MARK: - managers
     lazy var userManager: UserManaging = UserManager(dependencies: self)
+    lazy var classTypeManager: ClassTypeManaging = ClassTypeManager(dependencies: self)
+    lazy var fitnessClassManager: FitnessClassManaging = FitnessClassManager(dependencies: self)
+    lazy var instructorManager: InstructorManaging = InstructorManager(dependencies: self)
+    lazy var roomManager: RoomManaging = RoomManager(dependencies: self)
+    lazy var traineeManager: TraineeManaging = TraineeManager(dependencies: self)
 }
 
 extension AppDependencies: HasLoggerService {}
-extension AppDependencies: HasUserDefaultsStoraging {}
-extension AppDependencies: HasUserManager {}
+extension AppDependencies: HasNetworking {}
+extension AppDependencies: HasStorage {}
+extension AppDependencies: HasManagers {}
+extension AppDependencies: HasAPIServices {}
