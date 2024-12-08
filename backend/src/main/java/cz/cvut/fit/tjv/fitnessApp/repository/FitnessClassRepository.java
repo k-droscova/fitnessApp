af747328f295f0716ba3d4a7e01cca4719c331ff
Karolina Droscova <drosckar@fit.cvut.cz>
2024-12-08 13:30:38 +0100

    no enhacements

--- backend/src/main/java/cz/cvut/fit/tjv/fitnessApp/repository/FitnessClassRepository.java
+++ backend/src/main/java/cz/cvut/fit/tjv/fitnessApp/repository/FitnessClassRepository.java
@@ -13,12 +13,4 @@ public interface FitnessClassRepository extends CrudRepository<FitnessClass, Lon
     List<FitnessClass> findFitnessClassesByDate(LocalDate date);
     List<FitnessClass> findFitnessClassesByDateAndRoom_Id(LocalDate date, Long roomId);
     List<FitnessClass> findFitnessClassesByTimeBetweenAndDate(LocalTime timeAfter, LocalTime timeBefore, LocalDate date);
-    List<FitnessClass> findFutureClasses();
-    List<FitnessClass> findPastClasses();
-    List<FitnessClass> findFutureClassesForRoom(Long roomId);
-    List<FitnessClass> findPastClassesForRoom(Long roomId);
-    List<FitnessClass> findFutureClassesForInstructor(Long instructorId);
-    List<FitnessClass> findPastClassesForInstructor(Long instructorId);
-    List<FitnessClass> findFutureClassesForTrainee(Long traineeId);
-    List<FitnessClass> findPastClassesForTrainee(Long traineeId);
 }
